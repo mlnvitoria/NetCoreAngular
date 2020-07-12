39b40baa30219f069b7717d8c5828b78dfcdc7d6
Vitoria Mendes <vitoriamendes95@gmail.com>
2020-07-12 12:52:47 -0300

    Initial commit - API Project

---
+++ booksapi/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace booksapi.Models
+{
+    public class Category
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "The 'title' field is required")]
+        [MinLength(3, ErrorMessage = "The 'title' field is too short.")]
+        [MaxLength(150, ErrorMessage = "The 'title' field is too long.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The 'code' field is required")]
+        [MinLength(3, ErrorMessage = "The 'code' field is too short.")]
+        [MaxLength(100, ErrorMessage = "The 'code' field is too long.")]
+        public string UrlCode { get; set; }
+    }
+}

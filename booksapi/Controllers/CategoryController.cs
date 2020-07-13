2855a67a5b1914990f7c51d4013f90c6f07acd75
Vitoria Mendes <vitoriamendes95@gmail.com>
2020-07-12 21:32:58 -0300

    Category GET and POST working

---
+++ booksapi/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using booksapi.Models;
+using booksapi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace booksapi.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoryController : ControllerBase
+    {
+     
+        public CategoryService Service { get; set; }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get ([FromServices] BooksApiContext booksApiContext)
+        {
+            try
+            {
+                Service = new CategoryService(booksApiContext);
+
+                return await Service.GetCategoriesAsync();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { status = "error", message = e.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post([FromServices] BooksApiContext booksApiContext, [FromBody] Category category)
+        {
+            try
+            {
+                Service = new CategoryService(booksApiContext);
+
+                if (ModelState.IsValid)
+                {
+                    return await Service.PostAsync(category);
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new { status = "error", message = e.Message, detail = e.InnerException.Message });
+            }
+           
+        }
+
+    }
+}
---
+++ booksapi/Services/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using booksapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace booksapi.Services
+{
+    public class CategoryService
+    {
+        private BooksApiContext _Context { get; set; }
+        public CategoryService(BooksApiContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            var categories = await _Context.Categories.ToListAsync();
+
+            return categories;
+        }
+
+        public async Task<Category> PostAsync(Category category)
+        {
+            
+            category.CreatedAt = DateTime.Now;
+            category.UpdatedAt = DateTime.Now;
+
+            _Context.Categories.Add(category);
+            await _Context.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
